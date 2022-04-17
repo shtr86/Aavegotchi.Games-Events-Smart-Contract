aa9e9c36541ae0fc6f04ecbbe71c6c4658911efa
shtr86 <shtr86@gmail.com>
2022-04-17 18:45:47 +0430

    Update AGG Events.sol

--- AGG Events.sol
+++ AGG Events.sol
@@ -23,16 +23,15 @@ contract AGGSmartEvents {
     mapping(address => mapping(address => uint)) Balances;
 
     event WithdrawBalance(address indexed by, address indexed tokenaddress, uint256 value);
-    event EventNewSignUP(uint indexed eventid, uint indexed tokenid, address indexed playeraddress);
-    event EventNewSignOut(uint indexed eventid, uint indexed tokenid, address indexed playeraddress);
-    event EventRewarded(uint indexed eventid, uint indexed tokenid, address indexed winneraddress, uint poisiton, uint amount, address tokenaddress);
+    event EventNewSignUP(uint indexed eventid, uint indexed gotchiid, address indexed playeraddress);
+    event EventNewSignOut(uint indexed eventid, uint indexed gotchiid, address indexed playeraddress);
+    event EventRewarded(uint indexed eventid, uint indexed gotchiid, address indexed winneraddress, uint poisiton, uint amount, address tokenaddress);
     event EventStateChanged(uint indexed eventid, EventState oldstate, EventState newstate);
-    event EventConfirmation(uint indexed eventid, address indexed by, bool IsConfirmed);
+    event EventNewConfirmation(uint indexed eventid, address indexed by, bool IsConfirmed);
 
     enum EventState{
         SignUp, // event is in sigup state
         Active, // event is in on-going state
-        AwaitingLeaderboard, // event is ended. it's awaiting leaderboard positions.
         AwaitingConfirms, // positions are submitted. top players can vote to confirm the positions.
         AwaitingAdminValidation, // players voted... an admin must validate to start delivering the rewards.
         Withdrawable, // it passed all validations. top players can withdraw their rewards.
@@ -40,8 +39,15 @@ contract AGGSmartEvents {
         Invalidated // event admin invalidated this event for some reasons.
     }
 
-    struct Player {
+    struct Token{
         uint tokenID;
+        address tokenAddress;
+        bool withdrawable;
+        bool depositable;
+    }
+
+    struct Player {
+        uint gotchiid;
         uint position;
         address payable playerAddress;
         bool voted;
@@ -51,7 +57,7 @@ contract AGGSmartEvents {
     struct EventType {
         uint typeID;
         string eventTypeTitle;
-        address paymentTokenAddress;
+        uint paymentTokenID;
         uint paymentTokenAmount;
         uint activeDurationHours;
         uint confirmDurationHours;
@@ -63,7 +69,8 @@ contract AGGSmartEvents {
     }
 
     struct AGGEvent { // AaveGotchi.Games Events structure
-        mapping (uint => Player) players;
+        mapping (uint => Player) playersByGotchiID; // GotchiID => Player
+        mapping (uint => uint) GotchiIDByIndex; // Index => GotchiID
         uint playersSize;
         uint eventTypeID;
         EventState state;
@@ -79,8 +86,13 @@ contract AGGSmartEvents {
 
     uint internal lastEventID;
 
+    Token[] internal tokens;
     EventType[] internal eventTypes;
     AGGEvent[] internal events;
+
+    mapping (address => bool) validators;
+    mapping (address => bool) bannedAddresses;
+    mapping (uint => bool) bannedGotchis;
 
     modifier onlyDev() {
         require(msg.sender == DevWallet, "Not a Dev!");
@@ -88,12 +100,12 @@ contract AGGSmartEvents {
     }
 
     modifier onlyValidator(uint event_id, address senderAddress) {
-        require(senderAddress != address(0) && events[event_id].validator == senderAddress, "Not a validator!");
+        require(senderAddress != address(0) && validators[senderAddress] && events[event_id].validator == senderAddress, "Not a validator!");
         _;
     }
 
-    modifier onlyContestants(uint event_id, uint tokenID, address senderAddress) {
-        require(senderAddress != address(0) && events[event_id].players[tokenID].playerAddress == senderAddress, "Not a contestant!");
+    modifier onlyContestants(uint event_id, uint gotchiid, address senderAddress) {
+        require(senderAddress != address(0) && events[event_id].playersByGotchiID[gotchiid].playerAddress == senderAddress, "Not a contestant!");
         _;
     }
 
@@ -121,7 +133,6 @@ contract AGGSmartEvents {
         } else {
             _;
         }
-        
     }
 
     function changeDev(address _newDevWallet) public onlyDev validAddress(_newDevWallet) {
@@ -132,19 +143,29 @@ contract AGGSmartEvents {
         DevWallet = payable(msg.sender);
     }
 
+    function defineNewToken(address tokenAddress, bool withdrawable, bool depositable) onlyDev public {
+        uint newIndex = tokens.length;
+        tokens.push(Token(newIndex, tokenAddress, withdrawable, depositable));
+    }
+
+    function editToken(uint tokenID, address newAddress, bool withdrawable, bool depositable) onlyDev public {
+        require(tokenID >= 0 && tokenID < tokens.length, "No such token to edit!");
+        tokens[tokenID] = Token(tokenID, newAddress, withdrawable, depositable);
+    }
 
 
-    function defineNewEventType(string memory title, address paymentTokenAddress, uint tokenAmount, uint durationHours, uint confirmationHours, uint entrySize, uint confimationSize, uint confrimationHighestPosition, uint[3][] memory RewardDistributionRules) onlyDev lockUP public {
+    function defineNewEventType(string memory title, uint tokenID, uint tokenAmount, uint durationHours, uint confirmationHours, uint entrySize, uint confimationSize, uint confrimationHighestPosition, uint[3][] memory RewardDistributionRules) onlyDev lockUP public {
         uint newIndex = eventTypes.length;
         eventTypes.push();
 
         require(RewardDistributionRules.length > 0, "Please define the Reward distribution rules!");
+        require(tokenID >= 0 && tokenID < tokens.length, "No such coinID!");
 
         EventType storage tmpEventType = eventTypes[newIndex];
 
         tmpEventType.typeID = newIndex;
         tmpEventType.eventTypeTitle = title;
-        tmpEventType.paymentTokenAddress = paymentTokenAddress;
+        tmpEventType.paymentTokenID = tokenID;
         tmpEventType.paymentTokenAmount = tokenAmount;
         tmpEventType.activeDurationHours = durationHours;
         tmpEventType.confirmDurationHours = confirmationHours;
@@ -160,16 +181,30 @@ contract AGGSmartEvents {
 
     }
 
- 
-    function getEventData(uint eventID) public view returns(string[] memory) {
-    require(eventID >= 0 && eventID < events.length, "No such event!");
-    string[] memory output = new string[](events.length);
+    function setEventPositionDataAndEnd(uint eventID, uint[2][] memory eventPositions) onlyDev lockUP public {
+        require(eventID >= 0 && eventID < events.length, "No such event!");
+        require(events[eventID].state == EventState.Active, "Event Already Ended!");
 
-    for (uint i = 0; i < events.length; i++) {
-        output[i] = string(abi.encodePacked("[", events[eventID].players[i].tokenID, ",", events[eventID].players[i].position, ",", events[eventID].players[i].playerAddress, ",", events[eventID].players[i].voted, "]"));
+        events[eventID].state = EventState.AwaitingConfirms;
+
+        for (uint i = 0; i < eventPositions.length; i++) {
+            require(eventPositions[i].length > 1, "Please define the Reward distribution rules correctly!");
+            require(events[eventID].playersByGotchiID[eventPositions[i][0]].playerAddress != address(0), "TokenID Not Exists in this event");
+            events[eventID].playersByGotchiID[eventPositions[i][0]].position = eventPositions[i][1];
+        }
     }
 
-    return output;
+ 
+    function getEventPositionsData(uint eventID) public view returns(string[] memory) {
+        require(eventID >= 0 && eventID < events.length, "No such event!");
+        string[] memory output = new string[](events[eventID].playersSize);
+
+        for (uint i = 0; i < events[eventID].playersSize; i++) {
+            uint gotchiID = events[eventID].GotchiIDByIndex[i];
+            output[i] = string(abi.encodePacked("[", events[eventID].playersByGotchiID[gotchiID].gotchiid, ",", events[eventID].playersByGotchiID[gotchiID].position, ",", events[eventID].playersByGotchiID[gotchiID].playerAddress, ",", events[eventID].playersByGotchiID[gotchiID].voted, "]"));
+        }
+
+        return output;
     }
 
     function getEventTypesCount() public view returns(uint) {
@@ -180,10 +215,11 @@ contract AGGSmartEvents {
         return approvedAddresses[msg.sender];
     }
 
-    function signUp(uint eventTypeID, uint tokenID) lockUpEvent(eventTypeID) external {
+    function signUp(uint eventTypeID, uint gotchiid) lockUpEvent(eventTypeID) external {
         require(eventTypeID >= 0 && eventTypeID < eventTypes.length, "No such eventType!");
+        require(tokens[eventTypes[eventTypeID].paymentTokenID].depositable, "Token not depositable!");
 
-        IERC20 paymentToken = IERC20(eventTypes[eventTypeID].paymentTokenAddress);
+        IERC20 paymentToken = IERC20(tokens[eventTypes[eventTypeID].paymentTokenID].tokenAddress);
         uint amountToPay = eventTypes[eventTypeID].paymentTokenAmount;
         
         if (paymentToken.allowance(msg.sender, address(this))  >= amountToPay) {
@@ -197,7 +233,7 @@ contract AGGSmartEvents {
        
         Player memory tmpPlayer;
 
-        tmpPlayer.tokenID = tokenID;
+        tmpPlayer.gotchiid = gotchiid;
         tmpPlayer.position = 0;
         tmpPlayer.playerAddress = payable(msg.sender);
         tmpPlayer.voted = false;
@@ -210,11 +246,14 @@ contract AGGSmartEvents {
         }
 
         require(events[lastEventID].state == EventState.SignUp, "This event is not open for new signups! try again for a new one...");
-        require(events[lastEventID].players[tokenID].playerAddress == address(0), "This Gotchi is already registered in this event!");
+        require(events[lastEventID].playersByGotchiID[gotchiid].playerAddress == address(0), "This Gotchi is already registered in this event!");
 
-        events[lastEventID].players[tokenID] = tmpPlayer;
+        events[lastEventID].playersByGotchiID[gotchiid] = tmpPlayer;
+        events[lastEventID].GotchiIDByIndex[events[lastEventID].playersSize] = gotchiid;
         events[lastEventID].playersSize++;
   
+        emit EventNewSignUP(lastEventID, gotchiid, msg.sender);
+
         if (eventTypes[eventTypeID].entrySize == events[lastEventID].playersSize) {
             events[lastEventID].state = EventState.Active;
             lastEventID++; // new event will be created for the next signup if this(lastEventID's) event is not created yet...
